7a70accafe3ab1e77f8dc7f53c5e2dbe043c314f
Gerald <me@jacobford.co.uk>
2020-03-30 09:06:22 +0100

    feat(tooling): added TS

--- webpack.config.js
+++ webpack.config.js
@@ -5,34 +5,46 @@ const dist = path.resolve(__dirname, "dist");
 const crate = path.resolve(__dirname, "wasm");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
+  devtool: "source-map",
   entry: {
-    index: "./src/main.js"
+    index: "./src/main.tsx",
   },
   output: {
     path: dist,
-    filename: "[name].js"
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        use: "babel-loader"
-      }
-    ]
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+    ],
   },
   resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
     alias: {
-      Wasm: path.join(crate, "pkg")
-    }
+      Wasm: path.join(crate, "pkg"),
+    },
   },
   plugins: [
     new WasmPackPlugin({
       crateDirectory: crate,
-      outDir: path.join(crate, "pkg")
+      outDir: path.join(crate, "pkg"),
     }),
     new WebpackHtmlPlugin({
-      template: path.resolve(__dirname, "template.html")
-    })
-  ]
+      template: path.resolve(__dirname, "template.html"),
+    }),
+  ],
 };
