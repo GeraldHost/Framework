3539a55f5e7856a9a1238cc62415f62e66afaf41
Gerald <me@jacobford.co.uk>
2020-04-04 12:29:51 +0100

    feat(wasm): impl blackjack

--- wasm/src/blackjack.rs
+++ wasm/src/blackjack.rs
@@ -1,17 +1,39 @@
 use wasm_bindgen::prelude::*;
 
+use crate::deck::Deck;
 use crate::hand::Hand;
 
 #[wasm_bindgen]
-pub struct BlackJack {}
+pub struct BlackJack {
+  deck: Deck,
+  hands: Vec<Hand>,
+}
 
 impl BlackJack {
+  pub fn start(&mut self) {
+    self.deck = Deck::new();
+  }
+
+  pub fn deal_all_hands(&mut self, hands: Vec<Hand>) {
+    for hand in hands {
+      self.deck.deal(&mut hand, 2);
+    }
+  }
+
+  pub fn deal(&mut self, hand: Hand) {
+    self.deck.deal(&mut hand, 1);
+  }
+
   pub fn is_bust(hand: &mut Hand) -> bool {
-    let hand_value = hand.get_value();
-    if (hand_value > 21) {
+    let hand_value = hand.value();
+    if hand_value > 21 {
       true
     } else {
       false
     }
+  }
+
+  pub fn get_deck(&mut self) -> Deck {
+    self.deck
   }
 }
--- wasm/src/hand.rs
+++ wasm/src/hand.rs
@@ -7,13 +7,15 @@ pub struct Hand {
   cards: Vec<Card>,
 }
 
+// Posibly make hand a train and then implement
+// BlackjackHand as that will have hit, stand and bust concepts
 #[wasm_bindgen]
 impl Hand {
   pub fn new() -> Hand {
     Hand { cards: vec![] }
   }
 
-  pub fn get_cards(&self) -> JsValue {
+  pub fn cards(&self) -> JsValue {
     JsValue::from_serde(&self.cards).unwrap()
   }
 
@@ -21,7 +23,7 @@ impl Hand {
     self.cards.push(card);
   }
 
-  pub fn get_value(&mut self) -> u32 {
+  pub fn value(&mut self) -> u32 {
     let mut total: u32 = 0;
     for card in &self.cards {
       if let Ok(value) = card.rank.get_value() {
