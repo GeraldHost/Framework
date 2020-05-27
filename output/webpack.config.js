d1eacc08c8f9d1462f1bfd66215b37e297a97f79
Gerald <me@jacobford.co.uk>
2020-05-27 20:32:51 +0100

    feat(output): update webpack

--- output/webpack.config.js
+++ output/webpack.config.js
@@ -2,27 +2,25 @@ const path = require("path");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 const WebpackHtmlPlugin = require("html-webpack-plugin");
 
-const dist = path.resolve(__dirname, "..", "dist"); 
-const crate = path.resolve(__dirname, "core");
+const dist = path.resolve(__dirname, "..", "dist");
+const crate = path.resolve(__dirname);
 
 module.exports = {
-  mode: "development",             
+  mode: "development",
   devtool: "source-map",
-  entry: { 
-    index: "./main.js"
+  entry: {
+    index: "./main.js",
   },
   output: {
     path: dist,
-    filename: "[name].js"
+    filename: "[name].js",
   },
   plugins: [
     new WasmPackPlugin({
       crateDirectory: crate,
     }),
     new WebpackHtmlPlugin({
-      template: path.resolve(__dirname, "..", "template.html")
-    })
-  ]
+      template: path.resolve(__dirname, "..", "template.html"),
+    }),
+  ],
 };
-
-                                                                                        
