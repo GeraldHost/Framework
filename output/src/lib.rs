e09ddee374abd9be102e528f8a6f04ff8bad561a
Gerald <me@jacobford.co.uk>
2020-05-31 21:33:02 +0100

    feat(output): new lib

--- output/src/lib.rs
+++ output/src/lib.rs
@@ -7,23 +7,19 @@ pub fn run() -> Result<(), JsValue> {
 let mut s0 = State::new("Count".to_string());
 let mut s1 = State::new(0);
 let n0 = node("div")?;
-let n1 = node("p")?;
+let n1 = node("button")?;
 append(&n0, &n1);
-let t0 = text("text");
+let t0 = text(&s0.value().to_string());
 append_text(&n1, &t0);
-let n2 = node("button")?;
-append(&n0, &n2);
-let t1 = text(&s0.value().to_string());
-append_text(&n2, &t1);
-let t2 = text(": ");
-append_text(&n2, &t2);
-let t3 = text(&s1.value().to_string());
-append_text(&n2, &t3);
-s1.bind(NodeType::TextNode(t3));
+let t1 = text(": ");
+append_text(&n1, &t1);
+let t2 = text(&s1.value().to_string());
+append_text(&n1, &t2);
+s1.bind(NodeType::TextNode(t2));
 let callback = move |event: web_sys::Event| {
 s1.set(s1.value() + 1);
 };
-listener(&n2, "click", callback);
+listener(&n1, "click", callback);
 render(n0);
 Ok(())
 }
